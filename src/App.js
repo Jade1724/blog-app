6db57b3713cbbecf710104abcf0da902c8bb6735
Haruka Ichinose <hic21@uclive.ac.nz>
2022-06-01 02:23:42 +1200

    Fetching data on button click impelemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState} from 'react';
-
+import { useEffect } from 'react';
 
 import Users from './views/Users';
 import Posts from './views/Posts';
@@ -7,9 +7,9 @@ import Comments from './views/Comments';
 
 function App() {
 
-
+  const API_BASE_URL = 'http://jsonplaceholder.typicode.com/';
   const [view, setView] = useState('users');
-
+  const [items, setItems] = useState([]); 
   
   function handleViewUsersClick() {
     setView('users')
@@ -23,6 +23,26 @@ function App() {
     setView('comments')
   }
 
+  useEffect(() => {
+
+    
+    async function fetchItems() {
+      try {
+        const response = await fetch(API_BASE_URL + view);
+        if (!response.ok) throw Error('Failed to get response properly');
+        const jsonItems = await response.json();
+        
+        console.log(jsonItems);
+        setItems(jsonItems);
+        // setItems(JSON.parse(jsonItems))
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+    (async () => fetchItems())();
+    
+  }, [view]);
+
   return (
     <div>
       <h1>Hello World!</h1>
@@ -31,9 +51,14 @@ function App() {
         <div onClick={handleViewPostsClick}>posts</div>
         <div onClick={handleViewCommentsClick}>comments</div>
       </header>
-      { view == 'users' && <Users /> }
-      { view == 'posts' && <Posts />}
-      { view == 'comments' && <Comments />}
+
+      <div>
+        {items.map(item => <div>{item.name}</div>)}
+      </div>
+
+      { view === 'users' && <Users /> }
+      { view === 'posts' && <Posts />}
+      { view === 'comments' && <Comments />}
 
     </div>
   );
