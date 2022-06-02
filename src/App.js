0ae3c66a268e7af697066bed39806c1162ebbdb1
Haruka Ichinose <hic21@uclive.ac.nz>
2022-06-03 03:14:44 +1200

    Code cleanup and formatting

--- src/App.js
+++ src/App.js
@@ -1,56 +1,67 @@
-import { useState} from 'react';
-import { useEffect } from 'react';
+import { useState } from "react";
+import { useEffect } from "react";
 
-import Users from './views/Users';
-import Posts from './views/Posts';
-import Comments from './views/Comments';
+import Users from "./views/Users";
+import Posts from "./views/Posts";
+import Comments from "./views/Comments";
 
 function App() {
-
-  const API_BASE_URL = 'http://jsonplaceholder.typicode.com/';
-  const [view, setView] = useState('users');
+  const API_BASE_URL = "http://jsonplaceholder.typicode.com/";
+  const [view, setView] = useState("users");
   const [items, setItems] = useState([]);
-  
+
   function handleViewUsersClick() {
-    setView('users')
+    setView("users");
   }
 
   function handleViewPostsClick() {
-    setView('posts')
+    setView("posts");
   }
 
   function handleViewCommentsClick() {
-    setView('comments')
+    setView("comments");
   }
 
   useEffect(() => {
     async function fetchItems() {
       try {
         const response = await fetch(API_BASE_URL + view);
-        if (!response.ok) throw Error('Failed to get response properly');
+        if (!response.ok) throw Error("Failed to get response properly");
         const jsonItems = await response.json();
-        console.log(jsonItems);
         setItems(jsonItems);
       } catch (err) {
-        console.log(err.message)
+        console.log(err.message);
       }
     }
     (async () => fetchItems())();
-    
   }, [view]);
 
   return (
     <div>
       <header>
-        <button className={view === 'users' ? 'current' : null } onClick={handleViewUsersClick}>users</button>
-        <button className={view === 'posts' ? 'current' : null }onClick={handleViewPostsClick}>posts</button>
-        <button className={view === 'comments' ? 'current' : null }onClick={handleViewCommentsClick}>comments</button>
+        <button
+          className={view === "users" ? "current" : null}
+          onClick={handleViewUsersClick}
+        >
+          users
+        </button>
+        <button
+          className={view === "posts" ? "current" : null}
+          onClick={handleViewPostsClick}
+        >
+          posts
+        </button>
+        <button
+          className={view === "comments" ? "current" : null}
+          onClick={handleViewCommentsClick}
+        >
+          comments
+        </button>
       </header>
 
-      { view === 'users' && <Users items={items}/> }
-      { view === 'posts' && <Posts items={items}/> }
-      { view === 'comments' && <Comments items={items}/>}
-
+      {view === "users" && <Users items={items} />}
+      {view === "posts" && <Posts items={items} />}
+      {view === "comments" && <Comments items={items} />}
     </div>
   );
 }
